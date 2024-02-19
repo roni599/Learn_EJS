edf5165fba664c4a24f8a2e3abb474f6f99e3f4f
Ebrahim Khalil Roni <ekroni99@gmail.com>
2024-02-19 12:13:06 +0600

    create folder for EJS and install express,ejs,nodemon and setup server and creare a folder that's name is views and in the views folder create inde.ejs file and create from and somthing like that and then send data to the inde.ejs file from the index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const hostName = '127.0.0.1';
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.set('view engine', 'ejs');
+
+let program = [];
+app.get('/', (req, res) => {
+    res.render('index', { programs: program });
+});
+
+app.post('/', (req, res) => {
+    const plName = req.body.pName;
+    program.push(plName);
+    res.redirect('/');
+})
+app.listen(PORT, hostName, () => {
+    console.log(`Server is running at http://${hostName}:${PORT}`);
+});
